5d10fc1c5778e49b637b6bf7196659ac149e3d8e
tthornock <tthornock@byupathway.edu>
2025-09-07 14:00:47 -0600

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string again = "yes";
+        while (again == "yes")
+        {
+            int guess_count = 1;
+
+            Random randomGenerator = new Random();
+            int magic_numb = randomGenerator.Next(1, 100);
+
+
+            int guess = -1;
+
+            while (guess != magic_numb)
+            {
+                Console.WriteLine("Guess the magic number!");
+                Console.Write("What is your guess (between 1-100)? ");
+                string guess_set = Console.ReadLine();
+                guess = int.Parse(guess_set);
+
+                if (guess > magic_numb)
+                {
+                    Console.WriteLine("Lower");
+                    guess_count = 1 + guess_count;
+                }
+                else if (guess < magic_numb)
+                {
+                    Console.WriteLine("Higher");
+                    guess_count = 1 + guess_count;
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guess_count} trys!");
+
+                    Console.Write("do you want to paly again? ");
+                    again = Console.ReadLine();
+                }
+            }
+
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
