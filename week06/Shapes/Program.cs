a1b20a454e38f84222bdbbd6f93d6480543ad21d
tthornock <tthornock@byupathway.edu>
2025-10-09 10:43:22 -0600

    week 6 learing activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square sha1 = new Square("Green", 5);
+        shapes.Add(sha1);
+
+        Rectangle sha2 = new Rectangle("Blue", 3, 5);
+        shapes.Add(sha2);
+
+        Circle sha3 = new Circle("Purple", 2);
+        shapes.Add(sha3);
+
+
+        foreach (Shape sha in shapes)
+        {
+            string color = sha.GetColor();
+
+            double area = sha.GetArea();
+
+            Console.WriteLine($"The color is {color} and the area of the shape is {area}");
+        }
     }
 }
